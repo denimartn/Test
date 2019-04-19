5b62082644e5749ece956bd0099a71c4f3de2e84
Denise <43113570+denimartn@users.noreply.github.com>
2019-04-19 16:40:13 +0200

    Update 45.js

--- 45.js
+++ 45.js
@@ -1,6 +1,6 @@
 // 45. Write a JavaScript program to check two given integer values and return true if one of the number is 15 or if their sum or difference is 15.
 function fiftheen(n1, n2) {
-    return n1 === 15 || n2 === 15 || n1 + n2 === 15 || s === 15;
+    return n1 === 15 || n2 === 15 || n1 + n2 === 15 || n1 - n2 === 15;
 }
 
 console.log(fiftheen(10, 3))
