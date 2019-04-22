14ce4fd6c58b6f10988596b5eaceefb4a514849c
Denise <43113570+denimartn@users.noreply.github.com>
2019-04-22 23:40:00 +0200

    Add files via upload

---
+++ 73.js
@@ -0,0 +1,9 @@
+// 73. Write a JavaScript program to reverse the elements of a given array of integers length 3
+
+function reverse(arr) {
+    return arr.reverse();
+}
+
+console.log(reverse([5, 4, 3]))
+console.log(reverse([1, 0, -1]))
+console.log(reverse([2, 3, 1]))
