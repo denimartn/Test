47f11459d452ec697202037f1c9939da8c01cb50
Denise <43113570+denimartn@users.noreply.github.com>
2019-04-23 22:18:27 +0200

    Update 54.js

--- 54.js
+++ 54.js
@@ -4,7 +4,7 @@ function checkVowels(str) {
     let counter = 0;
     for(let i = 0; i < str.length; i++) {
         if(vowels.includes(str[i])) {
-            counter += 1;
+            counter ++;
         }
     }
     return counter;
