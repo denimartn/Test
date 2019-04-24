ca66c303ef0822acb920bffb4df86cf3676e6bd7
Denise <43113570+denimartn@users.noreply.github.com>
2019-04-24 22:59:47 +0200

    Add files via upload

---
+++ endsWith.js
@@ -0,0 +1,10 @@
+// Source: https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-65.php
+
+function endsWith(str) {
+    if(str.length >= 6) {
+        return str.slice(-6) === 'Script';
+    }
+}
+
+
+
