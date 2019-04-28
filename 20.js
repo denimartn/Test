6e86fda86fd5bda38466898459895262350a952b
Denise <43113570+denimartn@users.noreply.github.com>
2019-04-28 23:06:18 +0200

    Update 20.js

--- 20.js
+++ 20.js
@@ -1,6 +1,5 @@
-// 20. Write a JavaScript program to check from two given integers, if one is positive and one is negative.
+// Source: https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-20.php
+
 function posAndNeg(n1, n2) {
     return (n1 > 0 && n2 < 0) || (n1 < 0 && n2 > 0);
 }
-
-posAndNeg(1, -5);
