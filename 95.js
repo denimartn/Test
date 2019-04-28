1e4ade11852f24b96e0b4b082840b946b5ff6bc5
Denise <43113570+denimartn@users.noreply.github.com>
2019-04-28 23:13:04 +0200

    Update 95.js

--- 95.js
+++ 95.js
@@ -1,4 +1,4 @@
-// 95. Write a JavaScript program to replace all the numbers with a specified number of a given array of integers.
+// Source: https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-95.php
 
 function substitute(arr, n1, n2) {
     for(let i = 0; i < arr.length; i++) {
@@ -9,7 +9,3 @@ function substitute(arr, n1, n2) {
     return arr;
 
 }
-
-console.log(substitute([1,2,3,4,5,8], 2, 4))
-console.log(substitute([1, 2, 3, 2, 2, 8, 1, 9], 2, 5))
-console.log(substitute([1,2,5,62,94,10,100], 100, 9999))
