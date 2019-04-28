55229e5cd322a57deb1f6a4a5531b236e7d25325
Denise <43113570+denimartn@users.noreply.github.com>
2019-04-28 23:04:22 +0200

    Update 16.js

--- 16.js
+++ 16.js
@@ -1,5 +1,4 @@
-// 16. Write a JavaScript program to compute the sum of the two given integers. 
-// If the two values are same, then returns triple their sum.
+// Source: https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-16.php
 
 function triple(n1, n2) {
     if(n1 === n2) {
@@ -8,6 +7,3 @@ function triple(n1, n2) {
       return n1 + n2;
     }
 }
-
-triple(10, 10)
-    
