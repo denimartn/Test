486fdce5ee4b1ea976c5ee74835a7edbb0c7d343
Denise <43113570+denimartn@users.noreply.github.com>
2019-04-28 23:07:26 +0200

    Update 36.js

--- 36.js
+++ 36.js
@@ -1,9 +1,8 @@
-// 36. Write a JavaScript program to check if the last digit of the three given positive integers is same.
+// Source: https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-36.php
+
 function checkLastDigit(n1, n2, n3) {
     if (n1 < 0 || n2 < 0 || n3 < 0) {
         return false;
     }
     return n1 % 10 === n2 % 10 && n2 % 10 === n3 % 10;
 }
-
-checkLastDigit(20, 30, 400);
