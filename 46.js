a93f14299ba36b7e99f3ccfe7f55f65197e0c2fc
Denise <43113570+denimartn@users.noreply.github.com>
2019-04-28 23:09:22 +0200

    Update 46.js

--- 46.js
+++ 46.js
@@ -1,4 +1,5 @@
-// Write a JavaScript program to check two given non-negative integers and if one of the number (not both) is multiple of 7 or 11.
+// Source: https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-46.php
+
 function multipleOrNot(n1, n2) {
     if (n1 < 0 || n2 < 0) {
         return false;
@@ -13,7 +14,3 @@ function multipleOrNot(n1, n2) {
 
     return Boolean(isN1 ^ isN2);
 }
-
-console.log(multipleOrNot(14, 21));
-console.log(multipleOrNot(14, 20));
-console.log(multipleOrNot(16, 20));
